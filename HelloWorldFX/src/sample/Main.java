50303b19f32641d797df22360181ed4f71828e04
Sandesh Thapa <jungsandes@gmail.com>
2022-08-25 00:11:22 -0500

    basics

--- HelloWorldFX/src/sample/Main.java
+++ HelloWorldFX/src/sample/Main.java
@@ -2,8 +2,14 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,6 +17,17 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        /*GridPane root = new GridPane();
+        root.setAlignment(Pos.CENTER);
+        root.setVgap(10);
+        root.setHgap(10);
+
+        Label greetings = new Label("Welcome to JavaFX!");
+        greetings.setTextFill(Color.GREEN);
+        greetings.setFont(Font.font("Times New Roman", FontWeight.BOLD,70));
+        root.getChildren().add(greetings);*/
+
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 1024, 768));
         primaryStage.show();
